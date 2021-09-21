1b8be59dc4fd5eb0af96061ac0eb40b67f30e043
Vlad <juzopigeon@gmail.com>
2021-09-21 13:07:06 +0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const slidesCount = mainSlide.querySelectorAll(div).length
+
+let activeSlideIndex = 0
+
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+
+upBtn.addEventListener('click', () => {
+  changeSlide('up')
+})
+
+downBtn.addEventListener('click', () => {
+  changeSlide('down')
+})
+
+
+function changeSlide(direction) {
+    if (direction === 'up') {
+        activeSlideIndex++
+        if (activeSlideIndex === slidesCount) {
+          activeSlideIndex = 0
+        }
+    } else if (deirection === 'down') {
+      activeSlideIndex--
+      if (activeSlideIndex < 0) {
+        activeSlideIndex = slidesCount - 1
+      }
+    }
+
+    mainSlide.style.transform =  `translateY(-2000px)`
+}
